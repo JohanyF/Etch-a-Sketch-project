638b2a62650149234b50f50b50706c0b5e01b777
JohanyF <johanyf627@gmail.com>
2023-07-04 20:10:28 -0500

    Added comments explaining each function.

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const eraserBtn = document.querySelector("#eraser");
 const clearBtn = document.querySelector("#clear");
 const slider = document.querySelector("#slide");
 
+//function creates a grid for the Etch-a-Sketch.
 const createGrid = (grid) => {
     for(let r = 0; r < grid; r++) {
         const row = document.createElement('div');
@@ -21,6 +22,7 @@ const createGrid = (grid) => {
 
 };
 
+//function allows the user to color the Etch-a-Sketch black.
 const colorGridBlack = () => {
     const cols = document.querySelectorAll(".col");
 
@@ -31,6 +33,7 @@ const colorGridBlack = () => {
     })
 };
 
+//fucntion allows the user to pick any color for them to use in the Etch-a-Sketch.
 const colorPicker = () => {
     const color = document.querySelector("#color");
     const cols = document.querySelectorAll(".col");
@@ -46,11 +49,13 @@ const random = (number) => {
     return Math.floor(Math.random() * number);
 };
 
+//function returns a random rgb value.
 const randomColor = () => {
     const color = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
     return color;
 };
 
+//function allows for the user to enter into "rainbow mode" where every section they color will produce a random generated color. 
 const rainbowMode = () => {
     const cols = document.querySelectorAll(".col");
 
@@ -61,6 +66,7 @@ const rainbowMode = () => {
     })
 };
 
+//function allows the user to erase specific squares by changing the color to white. 
 const eraser = () => {
     const cols = document.querySelectorAll(".col");
 
@@ -71,23 +77,27 @@ const eraser = () => {
     })
 };
 
+//function removes all of the color in the Etch-a-Sketch by changing the color to white.
 const clearColor = () => {
     const cols = document.querySelectorAll(".col");
 
     cols.forEach(col => col.style.background = 'white');
 };
 
+//function deletes the grid of the Etch-a-Sketch.
 const removeGrid = () => {
    while(sketch.hasChildNodes()) {
     sketch.removeChild(sketch.firstChild);
    }
 };
 
+//function updates the text of the grid size after the user interacts with the slider.
 const updateGridSizeText = (gridSize) => {
     const gridSizeSelector = document.querySelector(".grid-size");
     gridSizeSelector.textContent = `${gridSize}x${gridSize}`;
 };
 
+//function is used to change the grid size based on the user iteraction with the slider. This generates a new grid for the user to color in the Etch-a-Sketch.
 const changeGrid = () => {
     const gridSizeNumber = document.querySelector("#slide").value;
     if(sketch.hasChildNodes()){
@@ -97,7 +107,7 @@ const changeGrid = () => {
     createGrid(gridSizeNumber);
 }
 
-
+//The Etch-a-Sketch automatically starts with a grid of 16x16 for the user to color with. 
 createGrid(16);
 
 blackBtn.addEventListener("click", colorGridBlack);
